de88934c17a17641060ef0df68d0a05245b929d7
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-17 10:55:10 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,18 @@
-// helloWorld function 
-function hello() {
+// // helloWorld function
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+// var helloWorld = function() {
+//     return "Hello, World!";
+// }
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+function helloWorld(){
     return "Hello, World!";
 }
+function sayHello() {
+
+}
+
